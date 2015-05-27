177bceecbf03a814820cb27e3bee4ff1efc1c128
cuitao <tao.cui@bjyada.com>
2015-05-27 18:04:13 +0800

    完善TLV

--- smart_card/pcsc/src/main/java/util/ConvertUtil.scala
+++ smart_card/pcsc/src/main/java/util/ConvertUtil.scala
@@ -2,15 +2,9 @@ package util
 
 import scala.collection.mutable.ListBuffer
 
-/**
- * Created by tao on 2015/5/27.
- */
 object ConvertUtil {
-  def bytes2Hex(bytes: Array[Byte]): String = {
-    bytes2Hex(bytes, " ")
-  }
 
-  def bytes2Hex(bytes: Array[Byte], sep: String): String = {
+  def bytes2Hex(bytes: Array[Byte], sep: String = " "): String = {
     bytes.map(_.formatted("%02X")).mkString(sep)
   }
 
@@ -18,7 +12,7 @@ object ConvertUtil {
     val hex = hexStr.replaceAll("\\s+", "")
     val listBuffer = new ListBuffer[Byte]
     for(g <- hex.grouped(2)) {
-      listBuffer += java.lang.Byte.valueOf(g, 16)
+      listBuffer += java.lang.Integer.valueOf(g, 16).toByte
     }
     listBuffer.toArray
   }
