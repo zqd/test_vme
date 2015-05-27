b353b54c5987fd7b35d39422f55046e13a0e0e1c
cuitao <tao.cui@bjyada.com>
2015-05-27 18:03:07 +0800

    完善TLV

--- smart_card/pcsc/src/main/java/ber/TLV.scala
+++ smart_card/pcsc/src/main/java/ber/TLV.scala
@@ -1,7 +1,32 @@
 package ber
 
-/**
- * Created by tao on 2015/5/27.
- */
-class TLV(tag: Tag, len: Int, value: Array[Byte]) {
+import scala.collection.mutable.ListBuffer
+
+class TLV private(val tag: Tag, val len: Int, value: Array[Byte], startIndex: Int = 0) {
+  private lazy val children = new ListBuffer[TLV]
+  val hasChild = tag.isStructObject && len > 0
+
+  if (hasChild) {
+    var index = startIndex + tag.getLen + 1
+    val endIndex = index + len
+
+    while(index < endIndex) {
+      val c = TLV(value, index)
+      add(c)
+      index = index + c.tag.getLen + 1 + c.len
+    }
+  }
+
+  def add(child: TLV): Unit = {
+    children += child
+  }
+}
+
+object TLV {
+  def apply(rawBytes: Array[Byte], startIndex: Int = 0): TLV = {
+    val len = rawBytes.indexWhere(!Tag.hasSubByte(_), startIndex) - startIndex + 1
+    val tag = Tag(rawBytes, startIndex, len)
+    val l = rawBytes(startIndex + len)
+    new TLV(tag, l, rawBytes, startIndex)
+  }
 }
--- smart_card/pcsc/src/main/java/ber/Tag.scala
+++ smart_card/pcsc/src/main/java/ber/Tag.scala
@@ -2,8 +2,13 @@ package ber
 
 import util.ConvertUtil
 
-class Tag private(rawTag: Array[Byte]) {
-  private val firstByte = ConvertUtil.getUnsignedValue(rawTag(0))
+class Tag private(rawTag: Array[Byte], startIndex: Int, len: Int) {
+  private val firstByte = ConvertUtil.getUnsignedValue(rawTag(startIndex))
+  private lazy val t = {
+    val cp = new Array[Byte](len)
+    rawTag.copyToArray(cp, startIndex)
+    cp
+  }
 
   lazy val level = {
     if ((firstByte & 0xC0) == 0) "通用级(Universal class)"
@@ -19,17 +24,21 @@ class Tag private(rawTag: Array[Byte]) {
     else "结构数据对象(Constructed data object)"
   }
 
-  def getTag = rawTag
+  def getTag = t
 
   def getHexTag = ConvertUtil.bytes2Hex(rawTag, "")
+
+  private[ber] def getLen = len
 
   override def toString = getHexTag
 }
 
 object Tag {
-  def apply(hexStr: String) = new Tag(ConvertUtil.hex2Bytes(hexStr))
+  def apply(hexStr: String): Tag = apply(ConvertUtil.hex2Bytes(hexStr))
 
-  def apply(rawTag: Array[Byte]) = new Tag(rawTag)
+  def apply(rawTag: Array[Byte]): Tag = apply(rawTag, 0, rawTag.length)
+
+  def apply(rawTag: Array[Byte], startIndex: Int, len: Int): Tag = new Tag(rawTag, startIndex, len)
 
   def hasSubByte(firstByte: Byte) = (ConvertUtil.getUnsignedValue(firstByte) & 0x1F) == 0x1F
 }
